4bcf47eb0ee4e96c599485152c30bbbb7b45f757
Hassan Khaled <58123330+HassanKha@users.noreply.github.com>
2024-02-10 17:27:41 +0200

    Update App.jsx

--- client/my-project/src/App.jsx
+++ client/my-project/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import UploadForm from './UploadForm'
 import axios from 'axios'
 function App() {
-  axios.defaults.baseURL = 'https://creativa.onrender.com/upload'
+  axios.defaults.baseURL = 'https://creativa.onrender.com'
   axios.defaults.withCredentials = true;
 
   return (
