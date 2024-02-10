7e980a064cb5b79f4b65e5f5105bc6d0d026931e
Hassan Khaled <58123330+HassanKha@users.noreply.github.com>
2024-02-10 16:29:55 +0200

    Update App.jsx

--- client/my-project/src/App.jsx
+++ client/my-project/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import UploadForm from './UploadForm'
 import axios from 'axios'
 function App() {
-  axios.defaults.baseURL = 'http://localhost:3000'
+  axios.defaults.baseURL = 'http://localhost:3001'
   axios.defaults.withCredentials = true;
 
   return (
